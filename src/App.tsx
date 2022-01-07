e7f8884abe530b6bee6a9433b0dcffad17f8290d
tienpvse1 <tienpvse@fpt.edu.vn>
2022-01-07 17:19:29 +0700

    resolved(layout):add link

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>hello world</h1>
+      <iframe
+        width="1024"
+        height="576"
+        src="https://www.youtube.com/embed/aCgP8BFjrw4"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
