96b6a3c09bd25452db10f899e5aa742d93ce0d45
tienpvse1 <tienpvse@fpt.edu.vn>
2022-01-08 13:48:59 +0700

    feature#6 - resolved(lint): change signle quote settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import "./App.css";
+import { useCookies } from 'react-cookie';
+import './App.css';
+import { TOKEN } from './constance/cookie';
 
 function App() {
+  const [cookie, setCookie] = useCookies();
+
+  const handleClick = () => {
+    setCookie(TOKEN, 'bearer ey');
+    console.log(cookie[TOKEN]);
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>hello world</h1>
+      <h1>{cookie[TOKEN] ? cookie[TOKEN] : 'undefind'}</h1>
+      <button onClick={handleClick}>set cookie</button>
     </div>
   );
 }
