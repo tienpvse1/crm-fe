8e2a72ba1207d9ce0b984974fee80358902031b3
tienpvse1 <tienpvse@fpt.edu.vn>
2022-01-08 13:32:05 +0700

    feature#6 - resolved(lib): add react-cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>hello world</h1>
-      <iframe width="900" height="506" src="https://www.youtube.com/embed/ctwHgxzdtr8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
 }
