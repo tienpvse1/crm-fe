4950670fbf1caefc140c5ac3a9f324311ec0b343
akiaki-47 <57090758+akiaki-47@users.noreply.github.com>
2022-01-11 23:11:28 +0700

    resolved(layout): layout header

--- src/common/header.tsx
+++ src/common/header.tsx
@@ -1,11 +1,20 @@
-import { Layout } from 'antd'
-
+import { Layout } from 'antd';
+import { MenuHeader } from '../components/header/menu-header';
 const { Header } = Layout;
+
+
 
 export const HeaderApp = () => {
     return (
-        <>
-            <Header className="site-layout-background" style={{ padding: 0 }} />
-        </>
+        <Header className="site-layout-background" style={{
+            display: 'flex',
+            backgroundColor: 'white',
+            height: '48px'
+        }}>
+            <div style={{ flex: '1 1 0%' }}></div>
+            <div>
+                <MenuHeader />
+            </div>
+        </Header>
     )
 }
