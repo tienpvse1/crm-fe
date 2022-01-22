ae0cdca3c0e0f689efdf52784fc370bcd5b41879
chuongnh4499 <nghuuchuong4499@gmail.com>
2022-01-22 22:55:48 +0700

    update logic unauthorize

--- src/common/admin-layout.tsx
+++ src/common/admin-layout.tsx
@@ -1,14 +1,20 @@
 import { MenuSiderAdmin } from '@components/layout/menu/admin-menu';
-import { PUBLIC_USER_INFO } from '@constance/cookie';
+import { PUBLIC_USER_INFO, TOKEN } from '@constance/cookie';
+import { getCookies } from '@cookies';
 import { Role } from '@interfaces/type-roles';
 import { useCookies } from 'react-cookie';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { LayoutApp } from './layout';
 
 export const LayoutAdmin = () => {
+  
   const [cookies] = useCookies([PUBLIC_USER_INFO]);
+  const token = getCookies(TOKEN);
+  if (!token[0]) return <Navigate to={'/login'} />;
+  // @ts-ignore
+  if (!token[0].token) return <Navigate to={'/login'} />;
 
-  if (cookies.public_user_info.role !== Role.ADMIN)
+  if (cookies.public_user_info?.role !== Role.ADMIN)
     throw new Error('Unauthorize');
 
 
--- src/common/layout.tsx
+++ src/common/layout.tsx
@@ -1,8 +1,5 @@
-import { TOKEN } from '@constance/cookie';
-import { getCookies } from '@cookies';
 import { Layout } from 'antd';
 import { ReactNode } from 'react';
-import { Navigate } from 'react-router-dom';
 import { ContentApp } from './app-content';
 import { SiderApp } from './sider';
 
@@ -12,11 +9,6 @@ interface LayoutAppProps {
 }
 
 export const LayoutApp = ({ content, menuSider }: LayoutAppProps) => {
-
-  const token = getCookies(TOKEN);
-  if (!token[0]) return <Navigate to={'/login'} />;
-  // @ts-ignore
-  if (!token[0].token) return <Navigate to={'/login'} />;
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
